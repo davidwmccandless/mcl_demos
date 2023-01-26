004f2c44f27f8a8fe2c4f9fa454e5533773985cc
davidwmccandless <112495960+davidwmccandless@users.noreply.github.com>
2023-01-26 16:53:54 -0600

    Collapsed four tables to two (two denormalized reporting tables to one, two data dictionaries to one) and hence moving two files to deprecated folder.
    also, moved the files to a new folder

--- airbnb_transactions.sql
+++ airbnb_transactions.sql
@@ -49,7 +49,8 @@ with rn_pre as
     , min(a."Start Date") over (partition by a.Listing,a.Guest) as guest_first_listing_visit_start_date
     , first_value(a."Confirmation Code") over (partition by a.Guest order by a."Start Date")=a."Confirmation Code" as guest_first_visit_bool
     , min(a."Start Date") over (partition by a.Guest) as guest_first_visit_start_date
-    , max(a.Date) over () as max_paid_out_date
+    , max(a.Date) over () as max_pay_out_date
+    , a."index"
     from
     reservation_history a
     left join rn
@@ -58,20 +59,155 @@ with rn_pre as
     left join reference_replace
     on a."Confirmation Code"=reference_replace."Confirmation Code"
 )
+,agg as
+(
+    select
+    "Confirmation Code"
+    ,Listing
+    ,date(min("Start Date")) as "Start Date"
+    ,max("Length of Stay") as mn
+    from
+    pre
+    where Listing is not null
+    group by 1,2
+)
+,agg_summarized as
+(
+    select
+    Listing
+    from
+    agg
+    group by 1
+)
+,dd as
+(
+    select
+    dd."Date"
+    ,a.Listing
+    from dim_date dd
+    left join agg_summarized a
+    on true
+)
+,pen as
+(
+    select
+    a."Confirmation Code"
+    ,a."Start Date"
+    ,date(a."Start Date",'+'||a.mn||' days') as last_night_of_stay
+    ,date(a."Start Date",'+'||(a.mn+1)||' days') as checkout_date
+    ,dd.Listing
+    ,date(dd.date) as Date
+    ,a.Listing is not null as occupied
+    ,a.mn as "Length of Stay"
+    ,max(date(a."Start Date",'+'||a.mn||' days')) over () as max_last_night_of_stay
+    ,row_number() over (partition by a."Confirmation Code") as rn
+    from dd
+    left join agg a
+    on a.Listing=dd.Listing
+    and dd.date between a."Start Date" and date(a."Start Date",'+'||a.mn||' days')
+    order by dd.Date
+)
+,checkout_dates as
+(
+    select
+    listing
+    ,checkout_date
+    from
+    pen
+    where listing is not null and checkout_date is not null
+    group by 1,2
+)
 select
-a.*
+pen."Confirmation Code"
+,pen."Start Date"
+,pen.last_night_of_stay
+,pen.Listing
+,pen.Date
+,pen.occupied
+,pen."Length of Stay"
+,pen.max_last_night_of_stay
+,pre."Payout Date"
+,pre.Type
+,pre."Guest ID"
+,pre.max_stays_by_single_guest_listing
+,pre.Currency
+,pre.Amount
+,pre."Paid Out"
+,pre."Airbnb Cut Host Fee"
+,pre."Cleaning Fee"
+,pre."Earnings Year"
+,pre.min_start_date
+,pre.max_start_date
+,pre.nights_for_occupancy
+,pre.guest_first_listing_visit_bool
+,pre.guest_first_listing_visit_start_date
+,pre.guest_first_visit_bool
+,pre.guest_first_visit_start_date
+,pre.max_pay_out_date
 ,case
-    when a.guest_first_listing_visit_bool
+    when pre.guest_first_listing_visit_bool
         then FALSE
     else
-        coalesce(a."Start Date">a.guest_first_listing_visit_start_date,FALSE)
+        coalesce(pre."Start Date">pre.guest_first_listing_visit_start_date,FALSE)
 end as guest_repeats_to_listing_bool
 ,case
-    when a.guest_first_visit_bool
+    when pre.guest_first_visit_bool
         then FALSE
     else
-        coalesce(a."Start Date">a.guest_first_visit_start_date,FALSE)
+        coalesce(pre."Start Date">pre.guest_first_visit_start_date,FALSE)
 end as guest_repeats_bool
+,pre."Referral Amount"
+,cd.Listing is not null as checkout_bool
 from
-pre a
+pen
+left join pre
+on pen.rn=1
+and
+pen."Confirmation Code"=pre."Confirmation Code"
+left join checkout_dates cd
+on pen.Listing=cd.Listing
+and pen.Date=cd.checkout_date
+where date<=max_last_night_of_stay
+
+union all
+
+-- now get referrals
+select
+null as "Confirmation Code"
+,null as "Start Date"
+,null as last_night_of_stay
+,null as Listing
+,pen.Date
+,null as occupied
+,null as "Length of Stay"
+,max_last_night_of_stay
+,pre."Payout Date"
+,pre.Type
+,null as "Guest ID"
+,null as max_stays_by_single_guest_listing
+,pre.Currency
+,pre.Amount
+,pre."Paid Out"
+,0 as "Airbnb Cut Host Fee"
+,0 as "Cleaning Fee"
+,pre."Earnings Year"
+,null as min_start_date
+,null as max_start_date
+,0 as nights_for_occupancy
+,null as guest_first_listing_visit_bool
+,null as guest_first_listing_visit_start_date
+,null as guest_first_visit_bool
+,null as guest_first_visit_start_date
+,pre.max_pay_out_date
+,null as guest_repeats_to_listing_bool
+,null as guest_repeats_bool
+,pre."Referral Amount"
+,null as checkout_bool
+from
+pen
+inner join pre
+on pen.rn=1
+and pre."Referral Amount">0
+and pen."Date"=date(pre."Payout Date")
+order by date
 ;
--- airbnb_transactions_dd.sql
+++ airbnb_transactions_dd.sql
@@ -7,26 +7,10 @@ cast('Type' as varchar(500)) as "Field Name"
 union all
 
 select
-cast('Confirmation Code' as varchar(500)) as "Field Name"
-,cast('dimension' as varchar(500)) as "Field Type"
-,cast('Numeric identifier unique to a stay - this value is generated in transformation and not an Airbnb confirmation code' as varchar(500)) as Description
-,'Numeric' as "Data Type"
-
-union all
-
-select
 cast('Start Date' as varchar(500)) as "Field Name"
 ,'dimension' as "Field Type"
 ,cast('Start date of stay' as varchar(500)) as Description
 ,'Date' as "Data Type"
-
-union all
-
-select
-cast('Guest ID' as varchar(500)) as "Field Name"
-,cast('dimension' as varchar(500)) as "Field Type"
-,cast('Numeric identifier unique to a guest name/listing combination - this value is generated in transformation and not an Airbnb confirmation code' as varchar(500)) as Description
-,'Numeric' as "Data Type"
 
 union all
 
@@ -57,7 +41,7 @@ union all
 select
 cast('Length of Stay' as varchar(500)) as "Field Name"
 ,'measure' as "Field Type"
-,cast('Length of stay' as varchar(500)) as Description
+,cast('Length of stay (nights)' as varchar(500)) as Description
 ,'Numeric' as "Data Type"
 
 union all
@@ -111,17 +95,9 @@ cast('Referral Amount' as varchar(500)) as "Field Name"
 union all
 
 select
-cast('Cleaning Fee' as varchar(500)) as "Field Name"
-,'measure' as "Field Type"
-,cast('The cleaning fee charged to the guest' as varchar(500)) as Description
-,'Numeric' as "Data Type"
-
-union all
-
-select
 cast('Occupancy' as varchar(500)) as "Field Name"
 ,'Calculated Measure' as "Field Type"
-,cast('Days Occupied / Count of Days between parameters [Occupancy Start Date] and [Occupancy End Date]' as varchar(500)) as Description
+,cast('Calculated at the [Listing] level: Days Occupied / Count of Days between parameters [Occupancy Start Date] and [Occupancy End Date]' as varchar(500)) as Description
 ,'Numeric' as "Data Type"
 
 union all
@@ -163,14 +139,6 @@ select
 cast('Distinct Guests' as varchar(500)) as "Field Name"
 ,'Calculated Measure' as "Field Type"
 ,cast('Count of distinct [Guest ID]' as varchar(500)) as Description
-,'Numeric' as "Data Type"
-
-union all
-
-select
-cast('Nights Occupied' as varchar(500)) as "Field Name"
-,'Calculated Measure' as "Field Type"
-,cast('Count of nights occupied' as varchar(500)) as Description
 ,'Numeric' as "Data Type"
 
 union all
@@ -224,17 +192,9 @@ cast('Host Referral Bonuses' as varchar(500)) as "Field Name"
 union all
 
 select
-cast('Nights Available' as varchar(500)) as "Field Name"
-,'Calculated Measure' as "Field Type"
-,cast('Count of nights available' as varchar(500)) as Description
-,'Numeric' as "Data Type"
-
-union all
-
-select
 cast('Date Filter' as varchar(500)) as "Field Name"
 ,'Calculated Measure' as "Field Type"
-,cast('TRUE if the earlier or [Start Date] or [Payout Date]>= parameter [Occupancy Start Date] and the greater of [Payout Date] or [Start Date]>=[Occupancy End Date]; else FALSE; note that if [Start Date] is null like with a referral, it is disregarded. ' as varchar(500)) as Description
+,cast('TRUE if [Date]>= parameter [Occupancy Start Date] and [Date]>=[Occupancy End Date]; else FALSE.' as varchar(500)) as Description
 ,'Boolean' as "Data Type"
 
 union all
@@ -252,5 +212,59 @@ cast('Cleaning Fee %' as varchar(500)) as "Field Name"
 ,'Calculated Measure' as "Field Type"
 ,cast('sum([Cleaning Fee])/[Total Host Payout]' as varchar(500)) as Description
 ,'Numeric' as "Data Type"
-;
 
+union all
+
+select
+cast('New Guests' as varchar(500)) as "Field Name"
+,'Calculated Measure' as "Field Type"
+,cast('count distinct [Guest ID] if the visit is the first visit for that [Guest ID]' as varchar(500)) as Description
+,'Numeric' as "Data Type"
+
+union all
+
+select
+cast('Repeat Business %' as varchar(500)) as "Field Name"
+,'Calculated Measure' as "Field Type"
+,cast('[Repeat Stays]/[Distinct Stays]' as varchar(500)) as Description
+,'Numeric' as "Data Type"
+
+union all
+
+select
+cast('Repeat Stays' as varchar(500)) as "Field Name"
+,'Calculated Measure' as "Field Type"
+,cast('count distinct [Confirmation Code] if the visit is the 2nd+ visit for that [Guest ID]' as varchar(500)) as Description
+,'Numeric' as "Data Type"
+
+union all
+
+select
+cast('Confirmation Code' as varchar(500)) as "Field Name"
+,cast('dimension' as varchar(500)) as "Field Type"
+,cast('Numeric identifier unique to a stay - this value is generated in transformation and not an Airbnb confirmation code' as varchar(500)) as Description
+,'Numeric' as "Data Type"
+
+union all
+
+select
+cast('last_night_of_stay' as varchar(500)) as "Field Name"
+,'dimension' as "Field Type"
+,cast('Ending date of stay' as varchar(500)) as Description
+,'Date' as "Data Type"
+
+union all
+
+select
+cast('Nights Available' as varchar(500)) as "Field Name"
+,'Calculated Measure' as "Field Type"
+,cast('Count of nights available' as varchar(500)) as Description
+,'Numeric' as "Data Type"
+
+union all
+
+select
+cast('Check Out Date' as varchar(500)) as "Field Name"
+,'dimension' as "Field Type"
+,cast('Check Out Date' as varchar(500)) as Description
+,'Date' as "Data Type";
