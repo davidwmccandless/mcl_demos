2d95e14617b4289ca8664f73eb2de59b328ae7ff
davidwmccandless <112495960+davidwmccandless@users.noreply.github.com>
2023-02-06 14:13:25 -0600

    adding quotation marks to timestamp
    dealing with datetime function ambiguity
    
    altering default file paths in case we're running this in Google Colab

--- airbnb_transaction_analysis/airbnb_transaction_etl.py
+++ airbnb_transaction_analysis/airbnb_transaction_etl.py
@@ -2,7 +2,7 @@
 import pandas
 import sys
 import sqlalchemy
-import datetime
+from datetime import datetime, timezone, timedelta
 import os
 import re
 
@@ -26,7 +26,7 @@ def extract_files(filenames):
     for match in matches:
         fpath = arg1+match.group(0) # details re: parsing CLI arguments from https://www.geeksforgeeks.org/command-line-arguments-in-python/
         ti_c = os.path.getctime(fpath) # get file creation epoch timestamp
-        c_ti = datetime.datetime.fromtimestamp(ti_c) # convert epoch timestamp to easily legible timestamp
+        c_ti = datetime.fromtimestamp(ti_c) # convert epoch timestamp to easily legible timestamp
 
         yield(
             pandas.read_csv(
@@ -54,30 +54,37 @@ df.head(0).copy().to_sql(sqlite_table, sqlite_connection, if_exists='replace')
 # now let's insert our rows
 df.to_sql(sqlite_table, sqlite_connection, if_exists='append')
 
-from datetime import datetime, timezone
+from datetime import datetime, timezone, timedelta
 
 dt = datetime.now(timezone.utc).strftime("%Y-%m-%d %H:%M:%S")
 
 # now let's insert a load timestamp column
-engine.execute("alter table reservation_history add column this_row_load_ts text default {0};".format(dt))
+engine.execute("alter table reservation_history add column this_row_load_ts text default '{0}';".format(dt))
 
 # 3. Load a dimensional date table to the database
 sqlite_table = "dim_date"
 min_date=df["Start Date"].min() # get minimum start of stay
 
 # get maximum start of stay + maximum length of nights across all stays
-max_date=df["Start Date"].max().to_pydatetime() + datetime.timedelta(days=(df["Nights"].max()))
+max_date=df["Start Date"].max().to_pydatetime() + timedelta(days=(df["Nights"].max()))
 max_date_trunc=max_date.year # get year of max_date
 
 # construct pandas dataframe between min_date and max stay date + end of following year
 df_date=pandas.DataFrame({"Date": pandas.date_range(min_date
-, datetime.datetime(max_date_trunc+1,12,31))})
+, datetime(max_date_trunc+1,12,31))})
 
 # write said pandas dataframe to SQLite database
 df_date.to_sql(sqlite_table, sqlite_connection, if_exists='replace')
 
 #4. Export the results of those queries to spreadsheets
-sql_files = os.listdir() # looks in the directory provided as argument or last token in CLI call
+arg0=sys.argv[0]
+
+if arg0.count('/')>0:
+    sql_dir=os.getcwd()+'/'+arg0.rsplit('/',1)[0]+'/'
+else:
+    sql_dir=os.getcwd()+'/'
+
+sql_files = os.listdir(sql_dir) # looks in the directory provided as argument or last token in CLI call
 
 # giving credit where it's due for the little utility below to read multiple CSVs to one pandas dataframe
 # https://pandasninja.com/2019/04/how-to-read-lots-of-csv-files-easily-into-pandas/
@@ -87,8 +94,9 @@ def load_files(filenames):
 
     with pandas.ExcelWriter("airbnb_transactions.xlsx") as writer:
         for match in matches:
+            i=match.group(0)
             # open text file in read mode
-            fn=match.group(0)
+            fn=sql_dir+i
             text_file = open(fn, "r")
         
             # read whole file to a string
@@ -98,10 +106,10 @@ def load_files(filenames):
             text_file.close()
 
             # get name of spreadsheet title from filename
-            fn=(fn.split('.'))[0]
+            sn=(i.split('.'))[0]
 
             # get select results to pandas dataframe + write to Excel
-            pandas.read_sql_query(sql_text, sqlite_connection).to_excel(writer, sheet_name=fn, index=False)
+            pandas.read_sql_query(sql_text, sqlite_connection).to_excel(writer, sheet_name=sn, index=False)
 
 load_files(sql_files)
 
